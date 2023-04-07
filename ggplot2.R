22c8510d58d4471eee4eb041b0c860239c877d0d
mduncan23 <109228421+mduncan23@users.noreply.github.com>
2023-04-07 13:25:36 -0400

    learning to chunk in markdown

---
+++ ggplot2.R
@@ -0,0 +1,14 @@
+Creating first markdown doc
+
+to add code snippets, use the green C in the top right of this window
+```{r}
+library(readr)
+library(ggplot2)
+tips <- read_csv('tips.csv')
+```
+
+markdown?
+```{r}
+
+```
+
