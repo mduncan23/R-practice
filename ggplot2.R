b4ef6e510b58d3802ea89d727be6aa0517aa998f
mduncan23 <109228421+mduncan23@users.noreply.github.com>
2023-04-12 10:43:54 -0400

    wrapping up first ggplot lesson

--- ggplot2.R
+++ ggplot2.R
@@ -46,11 +46,127 @@ tips %>%
 # mean by sex and time?
 # need to add both to same group by and not use two group bys or else it overrides
 tips %>% 
-  group_by(sex, time) %>% 
+  group_by(sex, time, smoker) %>% 
   # group_by(time) %>% 
-  summarise(mean(tip))
+  summarise(mean_tip = mean(tip))
 
 # k, back to plotting
 ggplot(tips, aes(x = total_bill,
                  y = tip,
                  color = time)) + geom_point()
+
+# adding in shape mapping
+ggplot(tips,
+       aes(x = total_bill,
+           y = tip,
+           color = time,
+           size = 20,
+           shape = smoker)) + geom_point()
+
+# checking unique days, 2 ways?
+tips %>% 
+  distinct(day)
+
+unique(tips['day'])
+
+# now make subplots!!
+ggplot(tips, 
+       aes(x = total_bill,
+           y= tip,
+           color = time, 
+           shape = smoker)) +
+  geom_point() +
+  facet_wrap(~day)
+
+# are there really no lunches on saturday?
+tips %>% 
+  filter(day == 'Sat') %>% 
+  group_by(time) %>% 
+  distinct(time)
+
+# lets move on to bar plots
+
+# if I use color, it just sets outlines
+ggplot(tips,
+       aes(x = day,
+           color = sex)) +
+  geom_bar()
+
+# I need to use fill to make a true stacked bar
+ggplot(tips,
+       aes(x = day,
+           fill = sex)) +
+  geom_bar()
+
+# if I want to specify a color I can do so 
+ggplot(tips,
+       aes(x = day,
+           fill = sex)) +
+  geom_bar() +
+  scale_fill_manual(values = c('deeppink', 'blue'))
+
+# if I want to make it a side by side bar
+ggplot(tips,
+       aes(y = day,
+           fill = sex)) +
+  geom_bar(position = 'dodge') +
+  scale_fill_manual(values = c('deeppink', 'blue'))
+
+# or if I want them but with a lil space between them
+ggplot(tips,
+       aes(x = day,
+           fill = sex)) +
+  geom_bar(position = 'dodge2') +
+  scale_fill_manual(values = c('deeppink', 'blue'))
+
+# If I want to make it proportional for each day
+ggplot(tips,
+       aes(x = day,
+           fill = sex)) +
+  geom_bar(position = 'fill') +
+  scale_fill_manual(values = c('deeppink', 'blue'))
+
+# now I want to do subplots
+ggplot(tips,
+       aes(x = day,
+           fill = sex)) +
+  geom_bar(position = 'dodge') +
+  facet_wrap(~time) + 
+  scale_fill_manual(values = c('deeppink', 'blue'))
+
+# can also make it confusing by adding element of smoker with outline
+ggplot(tips,
+       aes(x = day,
+           fill = sex,
+           alpha = smoker)) +
+  geom_bar(position = 'dodge') +
+  facet_wrap(~time) + 
+  scale_fill_manual(values = c('deeppink', 'blue'))
+
+
+# now to rename stuff and make the plot prettier
+ggplot(tips,
+       aes(x = day,
+           fill = sex)) +
+  geom_bar(position = 'dodge') +
+  facet_wrap(~time) +
+  scale_fill_manual(values = c('deeppink', 'blue')) +
+  labs(title = 'Number of Patrons by Day and Meal',
+       subtitle = 'Position by Gender',
+       x = 'Day',
+       y= 'Count')
+
+
+# now to put it all together with the pipeline
+tips %>% 
+  filter(smoker == 'Yes') %>% 
+  ggplot(aes(x = day,
+             fill = sex)) +
+  geom_bar(position = 'dodge') +
+  facet_wrap(~time) + 
+  scale_fill_manual(values = c('deeppink', 'blue')) +
+  labs(title = 'Number of Patrons who Smoke by Day and Meal',
+       subtitle = 'Position by Gender',
+       x = 'Day',
+       y = 'Count')
+
